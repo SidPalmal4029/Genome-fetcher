44c2b8e82dd1c9a007312753e3df2be0bc1f7f77
Siddhakam Palmal <sidpalmal@gmail.com>
2026-03-14 19:25:59 +0530

    Refactor verify.py for improved readability

--- commands/verify.py
+++ commands/verify.py
@@ -1,14 +1,11 @@
 from pathlib import Path
-
 from core.checksum import sha256sum
 from core.config import ConfigManager
 from core.remote_manager import RemoteManager
 
 
 def run_verify(args):
-
     config = ConfigManager(args.config)
-
     conn = {
         "host": config.host,
         "user": config.user,
@@ -16,19 +13,13 @@ def run_verify(args):
     }
 
     remote = RemoteManager(conn)
-
     local_file = Path(args.file)
-
     local_hash = sha256sum(local_file)
-
     remote_path = config.target + "/" + local_file.name
-
     remote_hash = remote.run(
         f"sha256sum {remote_path} | cut -d ' ' -f1"
     ).strip()
-
     if local_hash == remote_hash:
         print("Verification successful")
-
     else:
         print("Verification failed")
